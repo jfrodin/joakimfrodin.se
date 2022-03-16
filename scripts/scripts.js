7b91f591e2926ae6deb78ba53eaece2e03825edc
Joakim Frödin <joakim.frodin@gmail.com>
2022-03-16 08:02:43 +0100

    Update scripts.js

--- scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // Display alert message after toggle paragraphs
-    $(".toggle-btn").click(function () {
-        $("p").toggle(2000, function () {
-        });
-    });
-});
